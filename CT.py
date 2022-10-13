294a607e2104f9da50aef68ecc2d66d6aec3e8c3
Dinank Soni <98347106+DinankSoni@users.noreply.github.com>
2022-10-13 22:37:18 +0530

    1.1

--- CT.py
+++ CT.py
@@ -1,12 +1,12 @@
 def encrypt(text,s):
     result = ""
-     # transverse the plain text
+     # transfer the plain text
     for i in range(len(text)):
         char = text[i]
-        # Encrypt uppercase characters in plain text
+        # Encrypt uppercase characters 
         if (char.isupper()):
             result += chr((ord(char) + s-65) % 26 + 65)
-        # Encrypt lowercase characters in plain text
+        # Encrypt lowercase characters 
         else:
             result += chr((ord(char) + s - 97) % 26 + 97)
     return result
